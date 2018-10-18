60abdcf5f8c6accf9bc98f7e71bfe03ccd3b1062
karun <karun.antony@academytrainee.com>
2018-10-18 17:30:14 +0100

    login done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './containers/LoginPage';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+  
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
+  state = {
+    isLoading: true,
+    groups: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/api/people');
+    const body = await response.json();
+    this.setState({ people: body, isLoading: false });
+  }
+
   render() {
+    // const {people, isLoading} = this.state;
+
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+
+
+
+   
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          
+          
+          
+          <Route path="/" component={LoginPage} />
+
+        </div>
+      </Router>
+      
     );
   }
 }
